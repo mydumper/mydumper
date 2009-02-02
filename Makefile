9f796eca9ccd1c63d6832013632cbecdeb951de9
Domas Mituzas <domas.mituzas@gmail.com>
2009-02-02 20:16:08 +0200

    * rename files and program from 'dump' to 'mydumper', as it never grew outside single file * suggest --help in case of bad command line option, thats for those typing -h

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CFLAGS=`mysql_config --cflags` `pkg-config --cflags glib-2.0 gthread-2.0` -Wall -O3 -g
 LDFLAGS=`mysql_config --libs_r` `pkg-config --libs glib-2.0 gthread-2.0`
 
-all: dump.o dump
+all: mydumper
 
 clean:
-	rm -rf dump *~ *BAK *.dSYM *.o
+	rm -rf mydumper dump *~ *BAK *.dSYM *.o
 
 indent:
 	gnuindent -ts4 -kr -l200 dump.c
--- mydumper.c
+++ mydumper.c
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
         g_option_context_add_main_entries(context, entries, NULL);
         if (!g_option_context_parse(context, &argc, &argv, &error))
         {
-                g_print ("option parsing failed: %s\n", error->message);
+                g_print ("option parsing failed: %s, try --help\n", error->message);
                 exit (EXIT_FAILURE);
         }
         g_option_context_free(context);
