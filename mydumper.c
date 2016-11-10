92ebda5ceab2dd9456bb6289a7c9302265cbb574
Maximiliano Bubenick <max.bubenick@percona.com>
2016-11-10 11:20:02 -0300

    first file then group

--- mydumper.c
+++ mydumper.c
@@ -407,10 +407,10 @@ void *process_queue(struct thread_data *td) {
 	MYSQL *thrconn = mysql_init(NULL);
 	g_mutex_unlock(init_mutex);
 
-	mysql_options(thrconn,MYSQL_READ_DEFAULT_GROUP,"mydumper");
 	if (defaults_file != NULL) {
 		mysql_options(thrconn,MYSQL_READ_DEFAULT_FILE,defaults_file);
 	}
+	mysql_options(thrconn,MYSQL_READ_DEFAULT_GROUP,"mydumper");
 
 	if (compress_protocol)
 		mysql_options(thrconn,MYSQL_OPT_COMPRESS,NULL);
@@ -611,10 +611,10 @@ void *process_queue_less_locking(struct thread_data *td) {
 	MYSQL *thrconn = mysql_init(NULL);
 	g_mutex_unlock(init_mutex);
 
-	mysql_options(thrconn,MYSQL_READ_DEFAULT_GROUP,"mydumper");
 	if (defaults_file != NULL) {
 		mysql_options(thrconn,MYSQL_READ_DEFAULT_FILE,defaults_file);
 	}
+	mysql_options(thrconn,MYSQL_READ_DEFAULT_GROUP,"mydumper");
 
 	if (compress_protocol)
 		mysql_options(thrconn,MYSQL_OPT_COMPRESS,NULL);
@@ -952,10 +952,10 @@ MYSQL *create_main_connection()
 	MYSQL *conn;
 	conn = mysql_init(NULL);
 
-	mysql_options(conn,MYSQL_READ_DEFAULT_GROUP,"mydumper");
 	if (defaults_file != NULL) {
 		mysql_options(conn,MYSQL_READ_DEFAULT_FILE,defaults_file);
 	}
+	mysql_options(conn,MYSQL_READ_DEFAULT_GROUP,"mydumper");
 
 	if (!mysql_real_connect(conn, hostname, username, password, db, port, socket_path, 0)) {
 		g_critical("Error connecting to database: %s", mysql_error(conn));
