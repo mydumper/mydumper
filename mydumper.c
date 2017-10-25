d0336b1d1eaf412e6d5db5187d9844e1a62b3332
Bernard Spil <brnrd@FreeBSD.org>
2017-10-25 13:06:29 +0200

    Fix version string in output
    The patch tries to not use the server version string for the version
    output.
    MySQL has added LIBMYSQL_VERSION to mysql_version.h
    MariaDB has added MARIADB_VERSION_NUMBER_STR via an include and no
    longer declares MYSQL_SERVER_VERSION for the client.

--- mydumper.c
+++ mydumper.c
@@ -824,7 +824,7 @@ int main(int argc, char *argv[])
 	g_option_context_free(context);
 
 	if (program_version) {
-		g_print("mydumper %s, built against MySQL %s\n", VERSION, MYSQL_SERVER_VERSION);
+		g_print("mydumper %s, built against MySQL %s\n", VERSION, MYSQL_VERSION_STR);
 		exit (EXIT_SUCCESS);
 	}
 
--- myloader.c
+++ myloader.c
@@ -114,7 +114,7 @@ int main(int argc, char *argv[]) {
 	g_option_context_free(context);
 
 	if (program_version) {
-		g_print("myloader %s, built against MySQL %s\n", VERSION, MYSQL_SERVER_VERSION);
+		g_print("myloader %s, built against MySQL %s\n", VERSION, MYSQL_VERSION_STR);
 		exit(EXIT_SUCCESS);
 	}
 
