mydumper/mydumper

commit 37aa06686dc9ca97abbb335f8bd4b354a9694d7c

adding json support. Fixes #46 (#63)

mydumper.c
@@ -2811,9 +2811,11 @@ guint64 dump_table_data(MYSQL * conn, FILE *file, char *database, char *table, c
 				/* We reuse buffers for string escaping, growing is expensive just at the beginning */
 				g_string_set_size(escaped, lengths[i]*2+1);
 				mysql_real_escape_string(conn, escaped->str, row[i], lengths[i]);
+                                if (fields[i].type == MYSQL_TYPE_JSON) g_string_append(statement_row, "CONVERT(");
 				g_string_append_c(statement_row,'\"');
 				g_string_append(statement_row,escaped->str);
 				g_string_append_c(statement_row,'\"');
+                                if (fields[i].type == MYSQL_TYPE_JSON) g_string_append(statement_row, " USING UTF8MB4)");
 			}
 			if (i < num_fields - 1) {
 				g_string_append_c(statement_row,',');

commit 400b50efa0f85a6466c2567fe03d6eb1fdb59257

Fix compilation with older MySQL versions.
Commit 37aa06686dc9ca97abbb335f8bd4b354a9694d7c broke compilation under
at least Debian 9 and CentOS 6, as MYSQL_TYPE_JSON is not defined in the
MySQL versions they ship.

mydumper.c
@@ -53,6 +53,11 @@ const char BINLOG_DIRECTORY[]= "binlog_snapshot";
 const char DAEMON_BINLOGS[]= "binlogs";
 #endif
 
+/* Some earlier versions of MySQL do not yet define MYSQL_TYPE_JSON */
+#ifndef MYSQL_TYPE_JSON
+#define MYSQL_TYPE_JSON 245
+#endif
+
 static GMutex * init_mutex = NULL;
 
 /* Program options */