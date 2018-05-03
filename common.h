4a8097b9e55c6c88d06305f359ff0828547e9a21
Max Bube <max.bubenick@percona.com>
2018-05-03 12:51:44 -0300

    remove ssl arguments when -DWITH_SSL=OFF

--- common.h
+++ common.h
@@ -23,11 +23,13 @@ char *password=NULL;
 char *socket_path=NULL;
 char *db=NULL;
 char *defaults_file=NULL;
+#ifdef WITH_SSL
 char *key=NULL;
 char *cert=NULL;
 char *ca=NULL;
 char *capath=NULL;
 char *cipher=NULL;
+#endif
 
 gboolean askPassword=FALSE;
 guint port=0;
@@ -50,12 +52,14 @@ GOptionEntry common_entries[] =
         { "version", 'V', 0, G_OPTION_ARG_NONE, &program_version, "Show the program version and exit", NULL },
         { "verbose", 'v', 0, G_OPTION_ARG_INT, &verbose, "Verbosity of output, 0 = silent, 1 = errors, 2 = warnings, 3 = info, default 2", NULL },
         { "defaults-file", 0, 0, G_OPTION_ARG_FILENAME, &defaults_file, "Use a specific defaults file",  NULL },
-        { "ssl", 0, 0, G_OPTION_ARG_NONE, &ssl, "Connect using SSL", NULL},
+#ifdef WITH_SSL
+		{ "ssl", 0, 0, G_OPTION_ARG_NONE, &ssl, "Connect using SSL", NULL},
         { "key", 0, 0, G_OPTION_ARG_STRING, &key, "The path name to the key file", NULL },
         { "cert", 0, 0, G_OPTION_ARG_STRING, &cert, "The path name to the certificate file", NULL },
         { "ca", 0, 0, G_OPTION_ARG_STRING, &ca, "The path name to the certificate authority file", NULL },
         { "capath", 0, 0, G_OPTION_ARG_STRING, &capath, "The path name to a directory that contains trusted SSL CA certificates in PEM format", NULL },
         { "cipher", 0, 0, G_OPTION_ARG_STRING, &cipher, "A list of permissible ciphers to use for SSL encryption", NULL },
+#endif
         { NULL, 0, 0, G_OPTION_ARG_NONE,   NULL, NULL, NULL }
 };
 
