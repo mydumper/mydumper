2f338280a28c51b08907d3b6ecd81b95377ce95f
Morgan Tocker <tocker@gmail.com>
2018-10-01 23:17:39 +0000

    Simplified string concatenation

--- mydumper.c
+++ mydumper.c
@@ -525,16 +525,15 @@ void *process_queue(struct thread_data *td) {
 		// Worker threads must set their tidb_snapshot in order to be safe
 		// Because no locking has been used.
  
-		GString *query= g_string_sized_new(1024);
-		g_string_append_printf(query, "SET SESSION tidb_snapshot = '%s'", tidb_snapshot);
+		gchar *query= g_strdup_printf("SET SESSION tidb_snapshot = '%s'", tidb_snapshot);
 
 		g_message("Thread %d set to tidb_snapshot '%s'", td->thread_id, tidb_snapshot);
 
-		if (mysql_query(thrconn, query->str)) {
+		if (mysql_query(thrconn, query)) {
 			g_critical("Failed to set tidb_snapshot: %s", mysql_error(thrconn));
 			exit(EXIT_FAILURE);
 		}
-		g_string_free(query, TRUE);
+		g_free(query);
 	}
 
 	/* Unfortunately version before 4.1.8 did not support consistent snapshot transaction starts, so we cheat */
@@ -1396,24 +1395,23 @@ void start_dump(MYSQL *conn)
  
 				MYSQL_RES *result = mysql_store_result(conn);
 				MYSQL_ROW row = mysql_fetch_row(result); /* There should never be more than one row */ 
-				tidb_snapshot = row[0];
+				tidb_snapshot = g_strdup(row[0]);
 				mysql_free_result(result);
 			}
  
 		}
  
 	// Need to set the @@tidb_snapshot for the master thread
-	GString *query= g_string_sized_new(1024);
-	g_string_append_printf(query, "SET SESSION tidb_snapshot = '%s'", tidb_snapshot);
+	gchar *query= g_strdup_printf("SET SESSION tidb_snapshot = '%s'", tidb_snapshot);
 
 	g_message("Set to tidb_snapshot '%s'", tidb_snapshot);
 
-	if (mysql_query(conn, query->str)) {
+	if (mysql_query(conn, query)) {
 		g_critical("Failed to set tidb_snapshot: %s", mysql_error(conn));
 		exit(EXIT_FAILURE);
 	}
-	g_string_free(query, TRUE);
-	
+	g_free(query);
+
 	// select the db if applicable
 	if (db) {
 		mysql_select_db(conn, db);
