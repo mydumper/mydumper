52bef770c2ff8d6797a2a9426272c104f555a337
davidducos <david.ducos@gmail.com>
2021-10-29 13:24:07 +0000

    Adding ignore generated fields parameter

--- mydumper.c
+++ mydumper.c
@@ -99,6 +99,7 @@ guint snapshot_count= 2;
 guint snapshot_interval = 60;
 gboolean daemon_mode = FALSE;
 gboolean have_snapshot_cloning = FALSE;
+gboolean ignore_generated_fields = FALSE;
 
 gchar *ignore_engines = NULL;
 char **ignore = NULL;
@@ -273,6 +274,9 @@ static GOptionEntry entries[] = {
      "WSREP_SYNC_WAIT value to set at SESSION level", NULL},
     { "where", 0, 0, G_OPTION_ARG_STRING, &where_option,
       "Dump only selected records.", NULL },
+    { "ignore-generated-fields", 0, 0, G_OPTION_ARG_NONE, &ignore_generated_fields,
+      "Queries related to generated fields are not going to be executed."
+      "It will lead to restoration issues if you have generated columns", NULL },
     {NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL}};
 
 struct tm tval;
@@ -2271,6 +2275,8 @@ gboolean detect_generated_fields(MYSQL *conn, struct db_table *dbt) {
   MYSQL_ROW row;
 
   gboolean result = FALSE;
+  if (ignore_generated_fields)
+    return FALSE;
 
   gchar *query = g_strdup_printf(
       "select COLUMN_NAME from information_schema.COLUMNS where "
