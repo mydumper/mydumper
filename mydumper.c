62e879f6dd96b4cd97ae0c0075a2df7a00759ed8
David Ducos <david.ducos@gmail.com>
2021-11-30 17:01:01 -0300

    Update mydumper.c

--- mydumper.c
+++ mydumper.c
@@ -1097,6 +1097,7 @@ int main(int argc, char *argv[]) {
     int pos=0;
     stream=TRUE;
     db=tmpargv[1];
+
     if (tmpargc > 2 ){
       GString *s = g_string_new(tmpargv[2]);
       for (pos=3; pos<tmpargc;pos++){
