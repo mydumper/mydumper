ddeac7191879db6b6f3c33c1abff1296ed3d19ed
davidducos <david.ducos@gmail.com>
2022-01-19 19:50:31 +0000

    Filtering options in myloader

---
+++ src/tables_skiplist.h
@@ -0,0 +1,16 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Authors:        David Ducos, Percona (david dot ducos at percona dot com)
+*/
+
+void read_tables_skiplist(const gchar *filename, guint *errors);
+gboolean check_skiplist(char *database, char *table);
