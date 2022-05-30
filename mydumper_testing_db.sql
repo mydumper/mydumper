a4a318c99ac0196d3936ca5a85935773060a2376
David Ducos <david.ducos@gmail.com>
2022-05-30 11:00:44 -0300

    Create mydumper_testing_db.sql

---
+++ mydumper_testing_db.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS myd_test;
+CREATE DATABASE myd_test;
+-- Tables and views
+USE myd_test;
+source /tmp/mydumper_testing_db_template.sql ;
+
+DROP DATABASE IF EXISTS myd_test_no_fk;
+CREATE DATABASE myd_test_no_fk;
+USE myd_test_no_fk;
+source /tmp/mydumper_testing_db_template.sql ;
+
+ALTER TABLE myd_test_no_fk.pertest_child drop CONSTRAINT pertest_child_ibfk_1;
+
+-- Empty database
+DROP DATABASE IF EXISTS empty_db;
+CREATE DATABASE empty_db;
+
+-- Tablespaces
+
+
+
+-- Others
