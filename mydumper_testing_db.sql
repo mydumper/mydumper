6941e8b041628a8bc97df0d60c5ee9e12497ecbf
David Ducos <david.ducos@gmail.com>
2022-05-30 11:10:16 -0300

    Update mydumper_testing_db.sql

--- mydumper_testing_db.sql
+++ mydumper_testing_db.sql
@@ -2,12 +2,12 @@ DROP DATABASE IF EXISTS myd_test;
 CREATE DATABASE myd_test;
 -- Tables and views
 USE myd_test;
-source /tmp/mydumper_testing_db_template.sql ;
+source mydumper_testing_db_template.sql ;
 
 DROP DATABASE IF EXISTS myd_test_no_fk;
 CREATE DATABASE myd_test_no_fk;
 USE myd_test_no_fk;
-source /tmp/mydumper_testing_db_template.sql ;
+source mydumper_testing_db_template.sql ;
 
 ALTER TABLE myd_test_no_fk.pertest_child drop CONSTRAINT pertest_child_ibfk_1;
 
