5e6fb226e0c540821454528e8024331380d362ad
David Ducos <david.ducos@gmail.com>
2022-06-16 11:19:09 -0500

    Update mydumper_testing_db.sql

--- mydumper_testing_db.sql
+++ mydumper_testing_db.sql
@@ -10,6 +10,7 @@ USE myd_test_no_fk;
 source mydumper_testing_db_template.sql ;
 
 ALTER TABLE myd_test_no_fk.pertest_child drop CONSTRAINT pertest_child_ibfk_1;
+ALTER TABLE myd_test_no_fk.child drop CONSTRAINT child_ibfk_1;
 
 -- Empty database
 DROP DATABASE IF EXISTS empty_db;
