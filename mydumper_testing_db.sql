13f35ae69132376fb6d10dda8ebf65b7865a4763
davidducos <david.ducos@gmail.com>
2022-09-07 19:59:49 +0000

    Fixing some testings

--- mydumper_testing_db.sql
+++ mydumper_testing_db.sql
@@ -11,8 +11,7 @@ CREATE DATABASE myd_test_no_fk;
 USE myd_test_no_fk;
 source mydumper_testing_db_template.sql ;
 
-ALTER TABLE myd_test_no_fk.pertest_child drop CONSTRAINT pertest_child_ibfk_1;
-ALTER TABLE myd_test_no_fk.child drop CONSTRAINT child_ibfk_1;
+ALTER TABLE myd_test_no_fk.pertest_child drop foreign key pertest_child_ibfk_1;
 
 -- Empty database
 DROP DATABASE IF EXISTS empty_db;
--- mydumper_testing_db_template.sql
+++ mydumper_testing_db_template.sql
@@ -27,15 +27,19 @@ CREATE TABLE t_w_g (   `id` bigint(20) NOT NULL AUTO_INCREMENT,   `val` VARCHAR(
 INSERT into t_w_g (val) values ("asdfljlkjgsadklfjewtjgasd");
 
 CREATE TABLE `table_varchar_pk` (
-  `id` char(5) NOT NULL,
+  `id` char(5) NOT NULL COLLATE latin1_bin,
   `val` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `table_varchar_pk` VALUES("csdfg",3);
+INSERT INTO `table_varchar_pk` VALUES(binary "csdfg",3);
 INSERT INTO `table_varchar_pk` VALUES("bsdfg",3);
 INSERT INTO `table_varchar_pk` VALUES("asdfg",3);
 INSERT INTO `table_varchar_pk` VALUES("Öasdf",3);
+INSERT INTO `table_varchar_pk` VALUES(binary "Asdfg",3);
+INSERT INTO `table_varchar_pk` VALUES("Bsdfg",3);
+INSERT INTO `table_varchar_pk` VALUES("Csdfg",3);
+
 -- Tables with special characters
 CREATE table IF NOT EXISTS `mydumper_aipk_uuid_%` (id int primary key auto_increment, val varchar(36));
 INSERT into `mydumper_aipk_uuid_%` (val) values (uuid());
