6785ed5a8764331ee91173f3c7c51b9cf8adf2c2
Andrew Hutchings <andrew@linuxjedi.co.uk>
2022-12-21 12:28:40 +0000

    Minor fixes to messages
    Using `--version` now shows whether it is MariaDB or MySQL that we are
    build with. Verbose messages now consistently use `:`.

--- src/myloader_jobs_manager.c
+++ src/myloader_jobs_manager.c
@@ -167,7 +167,7 @@ void *loader_thread(struct thread_data *td) {
   if (td->thrconn)
     mysql_close(td->thrconn);
   mysql_thread_end();
-  g_debug("Thread %d ending", td->thread_id);
+  g_debug("Thread %d: ending", td->thread_id);
   return NULL;
 }
 
