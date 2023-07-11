20cf1f12e099aed48f947c7802a0ed9fc84731e0
davidducos <david.ducos@gmail.com>
2023-07-11 13:18:53 +0000

    Fixing detect server functionality

--- .circleci/circleci_config_builder.sh
+++ .circleci/circleci_config_builder.sh
@@ -46,7 +46,7 @@ all_vendors[${vendor}_1]="pingcap/tidb"
 list_all_vendors=( "percona57" "percona80" "mariadb1011" "mariadb1006")
 list_percona_version=( "percona57" "percona80" )
 list_mariadb_version=( "mariadb1004" "mariadb1005" "mariadb1006" "mariadb1011" )
-list_mariadb_version=( "mariadb1011" )
+list_mariadb_version=( "mariadb1011" "mariadb1006")
 # list_arch=( "arm" "amd" )
 list_arch=( "amd" )
 declare -A all_arch
