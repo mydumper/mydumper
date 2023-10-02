ae213df0dbe4bf0d15f6fb4437b98f021a463076
davidducos <david.ducos@gmail.com>
2023-10-02 16:51:03 +0000

    Testing jemalloc support

--- test_mydumper.sh
+++ test_mydumper.sh
@@ -10,7 +10,7 @@ stream_stor_dir="/tmp/stream_data"
 mydumper_base="."
 mydumper="${mydumper_base}/mydumper"
 myloader="${mydumper_base}/myloader"
-LD_PRELOAD=/usr/lib64/libjemalloc.a
+LD_PRELOAD=/usr/lib64/libjemalloc.so
 export LD_PRELOAD
 export G_DEBUG=fatal-criticals
 > $mydumper_log
