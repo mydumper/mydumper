3464b378b3bc0223edeb38a35ef2aed1d61ef4cb
Aleksey Midenkov <midenok@gmail.com>
2023-10-25 21:48:58 +0300

    Restore of views chain sometimes fail
    Related to CONTRIB-14 and multi-threaded creation of views. When one
    thread already dropped table v1 but did not yet created view v1
    another thread creates view v2 which selects from v1 and fails.
    
    This cannot be fixed with LOCK TABLES as lock is closed when table is
    dropped. This cannot be fixed with CREATE OR REPLACE as this doesn't
    work on different type (table cannot be replaced by view). As a quick
    fix we make --max-threads-for-post-actions 1 by default, the number of
    threads used for views creation.
    
    Bug description:
    
    https://jira.mariadb.org/browse/CONTRIB-17

--- src/myloader_arguments.c
+++ src/myloader_arguments.c
@@ -77,7 +77,7 @@ static GOptionEntry threads_entries[] = {
     {"max-threads-for-index-creation", 0, 0, G_OPTION_ARG_INT, &max_threads_for_index_creation,
      "Maximum number of threads for index creation, default 4", NULL},
     {"max-threads-for-post-actions", 0, 0, G_OPTION_ARG_INT,&max_threads_for_post_creation,
-     "Maximum number of threads for post action like: constraints, procedure, views and triggers, default 4", NULL},
+     "Maximum number of threads for post action like: constraints, procedure, views and triggers, default 1", NULL},
     {"max-threads-for-schema-creation", 0, 0, G_OPTION_ARG_INT, &max_threads_for_schema_creation,
      "Maximum number of threads for schema creation. When this is set to 1, is the same than --serialized-table-creation, default 4", NULL},
     {"exec-per-thread",0, 0, G_OPTION_ARG_STRING, &exec_per_thread,
