a0b536fb18a80fdc66308dd79cc3bf68f659b6de
davidducos <david.ducos@gmail.com>
2024-07-23 21:16:22 +0000

    Fixing multiple issues and adding more options to the metadata file

--- src/mydumper_working_thread.c
+++ src/mydumper_working_thread.c
@@ -558,11 +558,16 @@ void write_snapshot_info(MYSQL *conn, FILE *file) {
   }
 
   if (masterlog) {
-    fprintf(file, "[master]\n# Channel_Name = '' # It can be use to setup replication FOR CHANNEL\nFile = %s\nPosition = %s\nExecuted_Gtid_Set = %s\n",
-            masterlog, masterpos, mastergtid);
+    fprintf(file, "[master]\n# Channel_Name = '' # It can be use to setup replication FOR CHANNEL\n");
     if (source_data > 0){
-			fprintf(file, "myloader_exec_reset_slave = %d\nmyloader_exec_change_master = %d\nmyloader_exec_start_slave = %d\n",source_data==1?1:0,source_data==1?1:0,source_data==1?1:0);
-    }
+      fprintf(file, "#MASTER_HOST = \"%s\"\n#MASTER_USER = \"\"\n#MASTER_PASSWORD = \"\"\nMASTER_LOG_FILE = \"%s\"\nMASTER_LOG_POS = %s\n",
+          hostname,masterlog, masterpos);
+			fprintf(file, "myloader_exec_reset_slave = %d\nmyloader_exec_change_master = %d\nmyloader_exec_start_slave = %d\n",
+					source_data==1?1:0,source_data==1?1:0,source_data==1?1:0);
+    }else{
+      fprintf(file, "File = %s\nPosition = %s\nExecuted_Gtid_Set = %s\n",
+					masterlog, masterpos, mastergtid);
+		}
     g_message("Written master status");
   }
 
