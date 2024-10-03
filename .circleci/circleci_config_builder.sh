861d37c50a5eecfa5bd75ad5b11e3e460a93a7a2
davidducos <david.ducos@gmail.com>
2024-10-03 17:33:16 +0000

    Adding checksum text when release is uploaded

--- .circleci/circleci_config_builder.sh
+++ .circleci/circleci_config_builder.sh
@@ -559,7 +559,9 @@ echo '
     steps:
     - attach_workspace:
         at: /tmp/package
-        - run: ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -b "$( md5sum * ; sha1sum * ; sha256sum * )" -prerelease -draft -delete ${CIRCLE_TAG} /tmp/package'
+        - run:
+            command: |
+              ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -b "$( md5sum * ; sha1sum * ; sha256sum * )" -prerelease -draft -delete ${CIRCLE_TAG} /tmp/package'
 
 echo '
   publish-ubuntu-repository:
