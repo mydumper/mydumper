1ff14f85185567428e4280eb9d1f22fcbff033cc
davidducos <david.ducos@gmail.com>
2024-10-08 13:30:42 +0000

    cleaning up

--- src/mydumper_chunks.c
+++ src/mydumper_chunks.c
@@ -181,7 +181,6 @@ struct chunk_step_item * initialize_chunk_step_item (MYSQL *conn, struct db_tabl
                (!csi->chunk_step->integer_step.is_unsigned && (rows / gint64_abs(csi->chunk_step->integer_step.type.sign.max   - csi->chunk_step->integer_step.type.sign.min)   > (dbt->min_chunk_step_size==0?1000:dbt->min_chunk_step_size))
               )
               )){
-              g_message("Rows: %"G_GUINT64_FORMAT" | Max: %"G_GUINT64_FORMAT" | Min: %"G_GUINT64_FORMAT" | min_chunk_step_size: %ld",rows, csi->chunk_step->integer_step.type.unsign.max, csi->chunk_step->integer_step.type.unsign.min, dbt->min_chunk_step_size);
               csi->chunk_step->integer_step.min_chunk_step_size=1;
               csi->chunk_step->integer_step.is_step_fixed_length=TRUE;
               csi->chunk_step->integer_step.max_chunk_step_size=1;
