1a2d05d0db2c5ae17ff7ad5b3d1a058d3c6fc245
davidducos <david.ducos@gmail.com>
2024-12-25 23:00:20 +0000

    Moving el repos

--- .circleci/circleci_config_builder.sh
+++ .circleci/circleci_config_builder.sh
@@ -561,13 +561,13 @@ echo -n '
     - run:
         command: |
           cd mydumper_repo/
-          export DIR_SUFFIX="" && [ $(($(echo "${CIRCLE_TAG}" | cut -d'.' -f3 | cut -d'-' -f1)%2)) -eq 0 ] && export DIR_SUFFIX="testing"
+          export DIR_SUFFIX="." && [ $(($(echo "${CIRCLE_TAG}" | cut -d'.' -f3 | cut -d'-' -f1)%2)) -eq 0 ] && export DIR_SUFFIX="testing"
           export APT_REPO="main" && [ $(($(echo "${CIRCLE_TAG}" | cut -d'.' -f3 | cut -d'-' -f1)%2)) -eq 0 ] && export APT_REPO="testing"
           export REPREPRO_OPTIONS="" && [ $(($(echo "${CIRCLE_TAG}" | cut -d'.' -f3 | cut -d'-' -f1)%2)) -eq 0 ] && export REPREPRO_OPTIONS=" -C testing "
           export BASE_PATH=$(pwd)
           export UBUNTU_PATH="apt/ubuntu"
           export DEBIAN_PATH="apt/debian"
-          export YUM_PATH="yum/${DIR_SUFFIX}"
+          export YUM_PATH="yum"
           git config --global user.name "David Ducos"
           git config --global user.email "david.ducos@gmail.com"
           git checkout HEAD~ yum/rpmmacros
@@ -604,18 +604,22 @@ echo -n '
           rpm -q gpg-pubkey --qf "%{name}-%{version}-%{release} --> %{summary}\n"'
 
 for i in ${list_el_os_without_el[@]} ; do
+echo -n "
+          mkdir -p ${i}/"
+echo -n '${DIR_SUFFIX}/'
 echo -n '
           cp '
-echo -n "/tmp/package/mydumper*el${i}*rpm ${i}"
-
+echo -n "/tmp/package/mydumper*el${i}*rpm ${i}/"
+echo -n '${DIR_SUFFIX}/'
 echo -n "
-          cd ${i}
+          cd ${i}/"
+echo -n '${DIR_SUFFIX}/
           rpm --addsign  *.rpm
-          git add *.rpm"
+          git add *.rpm'
 echo -n '
           createrepo_c --update . 
           git add repodata
-          cd ..'
+          cd ${YUM_PATH}'
 done
 
 echo '
