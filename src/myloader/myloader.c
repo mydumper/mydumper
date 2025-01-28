657d507e44e91125dd64203bb7c5091c18a4e675
David Ducos <david.ducos@gmail.com>
2025-01-28 22:48:18 +0100

    Update myloader.c

--- src/myloader/myloader.c
+++ src/myloader/myloader.c
@@ -676,6 +676,7 @@ int main(int argc, char *argv[]) {
 
   if (key_file)  g_key_file_free(key_file);
   g_remove(fifo_directory);
+  g_message("Restore completed");
   return errors ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
