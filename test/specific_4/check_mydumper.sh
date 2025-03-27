9f16272f9b56c3d26c2c2283ab7ea79876c1f3c0
davidducos <david.ducos@gmail.com>
2025-03-27 12:52:47 +0000

    Inverting check

--- test/specific_4/check_mydumper.sh
+++ test/specific_4/check_mydumper.sh
@@ -1,6 +1,6 @@
 
 
-num_of_inserts=$(grep INSERT /tmp/data/specific_4.t_regex_partition.00000.sql | wc -l)
+num_of_inserts=$(grep INSERT /tmp/data/specific_4.t_regex_partition.*.sql | wc -l)
 
 if [ $num_of_inserts == 2  ]
 then
