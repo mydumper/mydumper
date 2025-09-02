f3e789ef559fbdc77807454fe9d33194faa1be1d
davidducos <david.ducos@gmail.com>
2025-09-02 21:34:30 +0000

    Fix Build on PS80, switch to MariaDB11

--- .circleci/circleci_config_builder.sh
+++ .circleci/circleci_config_builder.sh
@@ -162,7 +162,7 @@ list_build=(
   "el8_mysql84_x86_64"        "el8_mysql84_aarch64"
   "el9_mysql84_x86_64"        "el9_mysql84_aarch64"
   "bullseye_mysql84_amd64" 
-  "build_bullseye_debian_default_amd64"    "bookworm_mariadb1011_arm64"
+  "bullseye_debian_default_amd64"    "bookworm_mariadb1011_arm64"
   "trixie_debian_default_amd64" "trixie_debian_default_arm64"
 )
 
