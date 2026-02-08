58725e0665f0a3eb103aaab24a1bac32e0c2f0b5
davidducos <david.ducos@gmail.com>
2026-02-08 15:26:33 +0000

    Adding test over large multi column primary key tables

---
+++ test/specific_31/check_mydumper.sh
@@ -0,0 +1,17 @@
+
+
+
+LOG=/tmp/test_mydumper.log.tmp
+
+amount_of_threads_1=$(grep '`k1` = 1)' $LOG | cut -d'-' -f4 | cut -d':' -f1 | sort | uniq -c | wc -l)
+
+amount_of_threads_10M=$(grep '`k1` = 10000000' $LOG | cut -d'-' -f4 | cut -d':' -f1 | sort | uniq -c | wc -l)
+
+
+if [ ${amount_of_threads_1} < 2 ] || [ ${amount_of_threads_10M} < 2 ]
+then
+  exit 1
+else
+  exit 0
+fi
+
