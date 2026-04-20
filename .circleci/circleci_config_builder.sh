2afe31b6335e0186b7a695280b9954ee10f3ace3
davidducos <david.ducos@gmail.com>
2026-04-20 18:12:39 +0000

    Adding test that should trigger bug reported on 2246

--- .circleci/circleci_config_builder.sh
+++ .circleci/circleci_config_builder.sh
@@ -493,8 +493,8 @@ cat <<EOF
     - when:
         condition: << parameters.test >>
         steps:
-        - run: bash ./test_mydumper.sh -d SSL
         - run: bash ./test_mydumper.sh -c 34:50 --skip-dynamic -d SSL
+        - run: bash ./test_mydumper.sh -d SSL
     - store_artifacts:
         path: /tmp/stream.sql
         destination: stream.sql
