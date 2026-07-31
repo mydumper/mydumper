f6a25df3e8afb0217f749eb9714e56b50419b5c8
davidducos <david.ducos@gmail.com>
2026-07-31 19:55:32 +0000

    Adding Resolute Raccoon

--- .circleci/circleci_config_builder.sh
+++ .circleci/circleci_config_builder.sh
@@ -720,7 +720,7 @@ echo -n '
     docker:
       - image: mydumper/mydumper-builder-noble
     steps:
-	- run: sudo apt update
+    - run: sudo apt update
     - run: sudo apt install -y git dpkg-dev apt-utils createrepo-c rpm reprepro
     - attach_workspace:
         at: /tmp/package    
